9f517eb2d0719d08034ee56a8a4d01d9a17a3bd2
EugenHotaj <eugen_hotaj_91@hotmail.com>
2022-03-27 19:07:57 -0400

    [WIP] Start defining end-to-end experience.
    Still WIP and does not work, just checking in.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from automl import openml_utils
 from automl import pipeline
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from ray import serve
 import pandas as pd
 import ray
@@ -12,43 +12,68 @@ app = FastAPI()
 @serve.ingress(app)
 class Server:
     def __init__(self):
-        self.dataset = openml_utils.dataset_from_task(
-            task_id=31, test_fold=9, n_valid_folds=3
-        )
-        self.model = pipeline.Pipeline(
-            numerical_columns=self.dataset.numerical_cols,
-            categorical_columns=self.dataset.categorical_cols,
-            label_column=self.dataset.label_col,
-        )
+        self.features_table = {}
+        self.labels_table = {}
+        self.model = None
+
+    @app.post("models/fit")
+    async def fit_model(self):
+        """Debug route which causes a new model to be trained on observed data."""
         self.model.fit(self.dataset.train, self.dataset.valid)
 
-    @app.get("/predict")
-    def predict(self):
-        example = {
-            "checking_status": "<0",
-            "duration": 6,
-            "credit_history": "critical/other existing credit",
-            "purpose": "radio/tv",
-            "credit_amount": 1169,
-            "savings_status": "no known savings",
-            "employment": ">=7",
-            "installment_commitment": 4,
-            "personal_status": "male single",
-            "other_parties": "none",
-            "residence_since": 4,
-            "property_magnitude": "real estate",
-            "age": 67,
-            "other_payment_plans": "none",
-            "housing": "own",
-            "existing_credits": 2,
-            "job": "skilled",
-            "num_dependents": 1,
-            "own_telephone": "yes",
-            "foreign_worker": "yes",
-        }
+    @app.post("models/create")
+    async def create_model(self, request: Request):
+        """Creates a new model with the given schema."""
+        request = await request.json()
+        features = request["features"]
+        numerical_columns, categorical_columns = [], []
+        for name, type_ in features.items():
+            if type_ in (bool, int, str):
+                categorical_columns.append(name)
+            else:
+                numerical_columns.append(name)
+        label_column = request["label"]
+        self.model = pipeline.Pipeline(
+            numerical_columns=numerical_columns,
+            categorical_columns=categorical_columns,
+            label_column=label_column,
+        )
+        return {"id": 1}
+
+    @app.post("models/predict")
+    async def predict(self, request: Request):
+        """Stores the example and (possibly) returns model predictions."""
+        request = await request.json()
+
+        # Store the observed features so we can later join them with the label.
+        example = request["features"]
+        prediction_id = len(self.features_table)
+        self.features_table[prediction_id] = example
+
+        # Model has not been trained yet. Return empty probs and leave it to the user
+        # to handle this case.
+        if not self.model.model:
+            return {"prediction_id": prediction_id, "probs": {}}
+
+        # Predict on the example if a model has been trained.
         df = pd.DataFrame.from_records([example])
         probs = self.model.predict(df)[self.model.prediction_column].values
-        return {"prob": {"good": probs[0], "bad": 1.0 - probs[0]}}
+        return {
+            "prediction_id": prediction_id,
+            # TODO(ehotaj): Do not hardcode the credit-g label here.
+            "probs": {"good": probs[0], "bad": 1.0 - probs[0]},
+        }
+
+    @app.post("models/train")
+    async def train(self, request: Request):
+        """Stores label for an observed example so they can be joined later."""
+        request = await request.json()
+
+        # Store the observed label so we can later join it with the features.
+        prediction_id = request["prediction_id"]
+        label = request["label"]
+        self.labels_table[prediction_id] = label
+        return {}
 
 
 ray.init(address="auto", namespace="serve")
